2a2d073b157e2ac27f6238b788ae06d0078cc03d
Nedzib Sastoque <bsastoque@buk.co>
2023-09-22 13:38:36 -0500

    feat: Eliminar archivos innecesarios
    Se eliminaron los archivos .browserslistrc, .editorconfig, .gitignore, README.md y angular.json porque ya no son necesarios para el proyecto.

--- src/app/core/locales/exLinks.ts
+++
@@ -1,6 +0,0 @@
-export let links = {
-    linkedin: "https://www.linkedin.com/in/nedzib/",
-    twitter: "https://twitter.com/Ned_zib",
-    github: "https://github.com/nedzib",
-    hackster: "https://www.hackster.io/nedzib",
-}
